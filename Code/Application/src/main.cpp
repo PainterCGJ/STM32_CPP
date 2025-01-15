4d5f05df546fd101cf8e156384b688b1b4bed326
Mr-Painter <571928107@qq.com>
2025-01-15 17:21:31 +0800

    测试串口代码

--- Code/Application/src/main.cpp
+++ Code/Application/src/main.cpp
@@ -1,17 +1,22 @@
 #include <cstdio>
 #include "main.h"
+#include <vector>
+// #include <utility>
+
 int main(void)
 {
 	Uart_Dev com(USART1_PA9TX_PA10RX, 115200, 100);
-	com.send((uint8_t *)"Hello World!\r\n", 14);
+	com.set_debug();
+	// com.send((uint8_t *)"Hello World!\r\n", 14);
+	printf("Hello World!\r\n");
 	uint8_t rx;
 	while (1)
 	{
-		// if(com.recv_len())
-		// {
-		// 	rx = com.recv();
-		// 	com.send(&rx,1);
-		// }
+		if(com.recv_len())
+		{
+			rx = com.recv();
+			com.send(&rx,1);
+		}
 	}
 }
 void vApplicationStackOverflowHook(TaskHandle_t xTask,

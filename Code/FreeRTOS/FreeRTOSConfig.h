ece1c7441a94d34ef545ed9a2aa4733370e80b2b
PainterCGJ <571928107@qq.com>
2025-02-07 09:39:49 +0800

    修复rtos找不到SVC、Systick中断问题

--- Code/FreeRTOS/FreeRTOSConfig.h
+++ Code/FreeRTOS/FreeRTOSConfig.h
@@ -660,6 +660,12 @@ extern uint32_t SystemCoreClock;
 #define INCLUDE_xTaskGetHandle 0
 #define INCLUDE_xTaskResumeFromISR 1
 
+extern "C"
+{
+    void SVC_Handler(void);
+    void PendSV_Handler(void);
+    void SysTick_Handler(void);
+}
 #define vPortSVCHandler SVC_Handler
 #define xPortPendSVHandler PendSV_Handler
 // #define xPortSysTickHandler SysTick_Handler

cb88dd5946abb0dfc2af688e694a6461011faf81
PainterCGJ <571928107@qq.com>
2025-02-07 14:39:50 +0800

    重构uart_bsp，取消全局设备指针，定义成static成员。将成员函数的定义放在类定义内部

--- Code/BSP/uart/stm32f10x_uart_bsp.cpp
+++ Code/BSP/uart/stm32f10x_uart_bsp.cpp
@@ -2,10 +2,8 @@
 #include "sys_bsp.hpp"
 #include "priority.h"
 
-static Uart_Dev *usart1;
-static Uart_Dev *usart2;
-static Uart_Dev *usart3;
-static Uart_Dev *debug_port;
+Uart_Dev *Uart_Dev::__uart_dev[(int)UartID::U_NUM];
+uint8_t Uart_Dev::__bsp_init_flag = 0;
 
 const Uart_Dev::Uart_info USART1_PA9TX_PA10RX __attribute__((unused)) = {
     USART1,
@@ -39,120 +37,6 @@ const Uart_Dev::Uart_info USART3_PB10TX_PB11RX __attribute__((unused)) = {
     RCC_APB2Periph_GPIOB,
 };
 
-Uart_Dev::Uart_Dev(Uart_info info, uint32_t baudrate, uint16_t rx_max_size) : __rx_queue(rx_max_size)
-{
-    GPIO_InitTypeDef GPIO_InitStructure;
-    USART_InitTypeDef USART_InitStructure;
-    NVIC_InitTypeDef NVIC_InitStructure;
-    uint8_t NVIC_IRQChannel;
-    uint8_t priority;
-
-    __dev_info = info;
-    __rx_max_size = rx_max_size;
-
-    /* init gpio */
-    RCC_APB2PeriphClockCmd(__dev_info.rx_port_rcc, ENABLE);
-    RCC_APB2PeriphClockCmd(__dev_info.tx_port_rcc, ENABLE);
-
-    GPIO_InitStructure.GPIO_Pin = __dev_info.tx_pin;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init(__dev_info.tx_port, &GPIO_InitStructure);
-
-    GPIO_InitStructure.GPIO_Pin = __dev_info.rx_pin;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(__dev_info.rx_port, &GPIO_InitStructure);
-
-    switch ((int)(__dev_info.USARTx))
-    {
-    case (int)USART1:
-    {
-        NVIC_IRQChannel = USART1_IRQn;
-        priority = UASRT1_PRIORITY;
-        RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-        usart1 = this;
-        break;
-    }
-    case (int)USART2:
-    {
-        NVIC_IRQChannel = USART2_IRQn;
-        priority = UASRT2_PRIORITY;
-        RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-        usart2 = this;
-        break;
-    }
-    case (int)USART3:
-    {
-        NVIC_IRQChannel = USART3_IRQn;
-        priority = UASRT3_PRIORITY;
-        RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
-        usart3 = this;
-        break;
-    }
-    default:
-        break;
-    }
-
-    NVIC_InitStructure.NVIC_IRQChannel = NVIC_IRQChannel;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = priority;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
-
-    USART_InitStructure.USART_BaudRate = baudrate;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-    USART_Init(__dev_info.USARTx, &USART_InitStructure);
-    USART_ITConfig(__dev_info.USARTx, USART_IT_RXNE, ENABLE);
-
-    USART_Cmd(__dev_info.USARTx, ENABLE);
-    __if_init = 1;
-}
-
-void Uart_Dev::set_debug()
-{
-    debug_port = this;
-}
-
-void Uart_Dev::send(uint8_t *data, uint16_t len)
-{
-    for (uint16_t i = 0; i < len; i++)
-    {
-        USART_SendData(__dev_info.USARTx, data[i]);
-        while (USART_GetFlagStatus(__dev_info.USARTx, USART_FLAG_TXE) == RESET)
-            ;
-    }
-}
-
-void Uart_Dev::send(uint8_t data)
-{
-    USART_SendData(__dev_info.USARTx, data);
-    while (USART_GetFlagStatus(__dev_info.USARTx, USART_FLAG_TXE) == RESET)
-        ;
-}
-
-void Uart_Dev::isr_handler(uint8_t data)
-{
-    __rx_queue.push(data);
-}
-
-uint8_t Uart_Dev::recv()
-{
-    // uint8_t data = __rx_queue.front();
-    uint8_t data;
-    __rx_queue.pop(data);
-    return data;
-}
-
-uint16_t Uart_Dev::recv_len()
-{
-    return __rx_queue.size();
-}
-
 extern "C"
 {
     void USART1_IRQHandler(void)
@@ -160,7 +44,7 @@ extern "C"
         if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
         {
             uint8_t data = USART_ReceiveData(USART1);
-            usart1->isr_handler(data);
+            Uart_Dev::__uart_dev[(int)Uart_Dev::UartID::Uart1]->__rx_queue.push(data);
         }
     }
 
@@ -169,7 +53,7 @@ extern "C"
         if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
         {
             uint8_t data = USART_ReceiveData(USART2);
-            usart2->isr_handler(data);
+            Uart_Dev::__uart_dev[(int)Uart_Dev::UartID::Uart2]->__rx_queue.push(data);
         }
     }
 
@@ -178,13 +62,13 @@ extern "C"
         if (USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
         {
             uint8_t data = USART_ReceiveData(USART3);
-            usart3->isr_handler(data);
+            Uart_Dev::__uart_dev[(int)Uart_Dev::UartID::Uart3]->__rx_queue.push(data);
         }
     }
 }
 
 int std::fputc(int ch, std::FILE *f)
 {
-    debug_port->send(ch);
+    Uart_Dev::__uart_dev[(int)Uart_Dev::UartID::Degug_port]->send(ch);
     return ch;
 }
--- Code/BSP/uart/stm32f10x_uart_bsp.hpp
+++ Code/BSP/uart/stm32f10x_uart_bsp.hpp
@@ -4,9 +4,33 @@
 #include <queue>
 #include "sys_bsp.hpp"
 #include "rtos_bsp.hpp"
+#include "priority.h"
+
+extern "C"
+{
+    void USART1_IRQHandler(void);
+    void USART2_IRQHandler(void);
+    void USART3_IRQHandler(void);
+    int std::fputc(int ch, std::FILE *f);
+}
 class Uart_Dev
 {
 public:
+    friend void USART1_IRQHandler(void);
+    friend void USART2_IRQHandler(void);
+    friend void USART3_IRQHandler(void);
+    friend int std::fputc(int ch, std::FILE *f);
+
+    enum class UartID
+    {
+        Uart1,
+        Uart2,
+        Uart3,
+
+        Degug_port,
+        U_NUM,
+    };
+
     typedef struct
     {
         USART_TypeDef *USARTx;
@@ -19,16 +43,133 @@ public:
         uint32_t rx_pin;
         uint32_t rx_port_rcc;
     } Uart_info;
-    Uart_Dev():__if_init(0),__rx_queue(100){};
-    Uart_Dev(Uart_info info, uint32_t baudrate, uint16_t rx_max_size);
-    void send(uint8_t *data, uint16_t len);
-    void send(uint8_t data);
-    uint8_t recv();
-    uint16_t recv_len();
-    void isr_handler(uint8_t data);
-    void set_debug();
+
+    Uart_Dev(Uart_info info, uint32_t baudrate, uint16_t rx_max_size) : __rx_queue(rx_max_size)
+    {
+        GPIO_InitTypeDef GPIO_InitStructure;
+        USART_InitTypeDef USART_InitStructure;
+        NVIC_InitTypeDef NVIC_InitStructure;
+        uint8_t NVIC_IRQChannel;
+        uint8_t priority;
+
+        if (!__bsp_init_flag)
+        {
+            __bsp_init();
+        }
+        __dev_info = info;
+        __rx_max_size = rx_max_size;
+
+        /* init gpio */
+        RCC_APB2PeriphClockCmd(__dev_info.rx_port_rcc, ENABLE);
+        RCC_APB2PeriphClockCmd(__dev_info.tx_port_rcc, ENABLE);
+
+        GPIO_InitStructure.GPIO_Pin = __dev_info.tx_pin;
+        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+        GPIO_Init(__dev_info.tx_port, &GPIO_InitStructure);
+
+        GPIO_InitStructure.GPIO_Pin = __dev_info.rx_pin;
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+        GPIO_Init(__dev_info.rx_port, &GPIO_InitStructure);
+
+        switch ((int)(__dev_info.USARTx))
+        {
+        case (int)USART1:
+        {
+            NVIC_IRQChannel = USART1_IRQn;
+            priority = UASRT1_PRIORITY;
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+            __index = (int)UartID::Uart1;
+            break;
+        }
+        case (int)USART2:
+        {
+            NVIC_IRQChannel = USART2_IRQn;
+            priority = UASRT2_PRIORITY;
+            RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+            __index = (int)UartID::Uart2;
+            break;
+        }
+        case (int)USART3:
+        {
+            NVIC_IRQChannel = USART3_IRQn;
+            priority = UASRT3_PRIORITY;
+            RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+            __index = (int)UartID::Uart3;
+            break;
+        }
+        default:
+            break;
+        }
+        __uart_dev[__index] = this;
+
+        NVIC_InitStructure.NVIC_IRQChannel = NVIC_IRQChannel;
+        NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = priority;
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+        NVIC_Init(&NVIC_InitStructure);
+
+        USART_InitStructure.USART_BaudRate = baudrate;
+        USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+        USART_InitStructure.USART_StopBits = USART_StopBits_1;
+        USART_InitStructure.USART_Parity = USART_Parity_No;
+        USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+        USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+        USART_Init(__dev_info.USARTx, &USART_InitStructure);
+        USART_ITConfig(__dev_info.USARTx, USART_IT_RXNE, ENABLE);
+
+        USART_Cmd(__dev_info.USARTx, ENABLE);
+    }
+
+    void send(uint8_t *data, uint16_t len)
+    {
+        for (uint16_t i = 0; i < len; i++)
+        {
+            USART_SendData(__dev_info.USARTx, data[i]);
+            while (USART_GetFlagStatus(__dev_info.USARTx, USART_FLAG_TXE) == RESET)
+                ;
+        }
+    }
+
+    void send(uint8_t data)
+    {
+        USART_SendData(__dev_info.USARTx, data);
+        while (USART_GetFlagStatus(__dev_info.USARTx, USART_FLAG_TXE) == RESET)
+            ;
+    }
+
+    uint8_t recv()
+    {
+        uint8_t data;
+        __rx_queue.pop(data);
+        return data;
+    }
+
+    uint16_t recv_len()
+    {
+        return __rx_queue.size();
+    }
+
+    void set_debug()
+    {
+        __uart_dev[(int)UartID::Degug_port] = this;
+    }
+
 private:
-    uint8_t __if_init;
+    static Uart_Dev *__uart_dev[(int)UartID::U_NUM];
+    static uint8_t __bsp_init_flag;
+
+    static void __bsp_init()
+    {
+        __bsp_init_flag = 1;
+        for (uint8_t i = 0; i < (int)UartID::U_NUM; i++)
+        {
+            __uart_dev[i] = nullptr;
+        }
+    }
+
+    uint8_t __index;
     Uart_info __dev_info;
     RTOS::queue<uint8_t> __rx_queue;
     // std::queue<uint8_t, std::deque<uint8_t, OSAllocator<uint8_t>>> __rx_queue;

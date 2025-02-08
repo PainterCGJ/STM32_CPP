6384c662ce312e51f5aafe0c8655314d66b40bfa
PainterCGJ <571928107@qq.com>
2025-02-08 17:44:26 +0800

    测试代码

--- Code/Application/src/main.cpp
+++ Code/Application/src/main.cpp
@@ -1,25 +1,20 @@
 #include <cstdio>
 #include "main.h"
-
+// #include <thread>
 using namespace RTOS;
- void timer_callback(void *arg)
+void timer_callback(void *arg)
 {
 	(*((int *)arg))++;
 	printf("%d\r\n", (*((int *)arg)));
 }
-int a = 0;
-Timer_Dev::Timer_info init_info = {
-	TIM2,
-	timer_callback,
-	&a,
-};
 
-void com_task(void* parg)
+void com_task(void *parg)
 {
 	Uart_Dev com(USART1_PA9TX_PA10RX, 115200, 100);
 	com.set_debug();
 	printf("hello world\r\n");
 	uint8_t rx;
+	// COM.kill();
 	while (1)
 	{
 		while (com.recv_len())
@@ -27,28 +22,42 @@ void com_task(void* parg)
 			rx = com.recv();
 			com.send(&rx, 1);
 		}
-		// printf("com\r\n");
 		delay_ms(100);
-		
 	}
 }
-int main(void)
+
+void system_config(void)
 {
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 	systick_init();
-	Thread COM(com_task, "com", 1);
-	// Uart_Dev com(USART1_PA9TX_PA10RX, 115200, 100);
-	// com.set_debug();
-	//xTaskCreate(com_task, "com", 128, NULL, 10, handler);
-//	com.send((uint8_t *)"Hello World!\r\n", 14);
-//	Timer_Dev timer(init_info, 2000000);
-//	timer.enable();
-	
-	RTOS::os_start_scheduler();
-	while (1)
+}
+void rtos_main(void)
+{
+	Thread COM(com_task, "com", 1, 100);
+
+	int a = 10;
+	list<int> li;
+	li.push_back(a);
+	a=20;
+	li.push_back(a);
+	li.print();
+	// int &b=li.back();
+	printf("b=%d\r\n",li.back());
+	printf("b=%d\r\n",li.front());
+
+
+	while(1)
 	{
-		
+		//delay_ms(1000);
 	}
+	// Timer_Dev::Timer_info init_info = {
+	// 	TIM2,
+	// 	timer_callback,
+	// 	&a,
+	// };
+	// Timer_Dev timer(init_info, 2000000);
+	// timer.enable();
+	// RTOS::os_start_scheduler();
 }
 void vApplicationStackOverflowHook(TaskHandle_t xTask,
 								   char *pcTaskName)

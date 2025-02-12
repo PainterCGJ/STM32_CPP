ef9c55718c33521554fdb24204200d713d866fb5
PainterCGJ <571928107@qq.com>
2025-02-12 14:18:33 +0800

    增大total_heap_size到10k，减小最小任务堆栈到64word

--- Code/FreeRTOS/FreeRTOSConfig.h
+++ Code/FreeRTOS/FreeRTOSConfig.h
@@ -117,7 +117,7 @@ extern uint32_t SystemCoreClock;
  * (in words, not in bytes!).  The kernel does not use this constant for any other
  * purpose.  Demo applications use the constant to make the demos somewhat portable
  * across hardware architectures. */
-#define configMINIMAL_STACK_SIZE 128
+#define configMINIMAL_STACK_SIZE 64
 
 /* configMAX_TASK_NAME_LEN sets the maximum length (in characters) of a task's
  * human readable name.  Includes the NULL terminator. */
@@ -285,7 +285,7 @@ extern uint32_t SystemCoreClock;
  * or heap_4.c are included in the build.  This value is defaulted to 4096 bytes but
  * it must be tailored to each application.  Note the heap will appear in the .bss
  * section.  See https://www.freertos.org/a00111.html. */
-#define configTOTAL_HEAP_SIZE (8 * 1024)
+#define configTOTAL_HEAP_SIZE (10 * 1024)
 
 /* Set configAPPLICATION_ALLOCATED_HEAP to 1 to have the application allocate
  * the array used as the FreeRTOS heap.  Set to 0 to have the linker allocate the
